e67be9f79e6658470366ef3a5cefe111dd7101ca
Luka Klacar <lukaklacar@gmail.com>
2021-01-04 18:50:35 +0100

    Fixed native build

--- src/code/mp3code/csbtl3.c
+++ src/code/mp3code/csbtl3.c
@@ -43,11 +43,7 @@ void sbt_mono_L3(float *sample, short *pcm, int ch)
 	for (i = 0; i < 18; i++)
 	{
 		fdct32(sample, pMP3Stream->vbuf + pMP3Stream->vb_ptr);
-#ifndef __EMSCRIPTEN__
-		window(pMP3Stream->vbuf, pMP3Stream->vb_ptr, pcm);
-#else
 		window2(pMP3Stream->vbuf, pMP3Stream->vb_ptr, pcm);
-#endif
 		sample += 32;
 		pMP3Stream->vb_ptr = (pMP3Stream->vb_ptr - 32) & 511;
 		pcm += 32;
